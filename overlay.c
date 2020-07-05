20a6ca163e738ae85f22b69586ec4f6dded5905e
Murloc Knight <murlockkinght@gmail.com>
2020-07-05 21:12:23 +0700

    config added

--- overlay.c
+++ overlay.c
@@ -8,6 +8,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define MAX_LEN 256
+
 static DB_functions_t *deadbeef;
 
 Display* display;
@@ -15,8 +17,11 @@ Window root;
 int default_screen;
 XSetWindowAttributes attrs;
 XVisualInfo vinfo;
-int c; 
 int width;
+int height = 50;
+char title_format[MAX_LEN];
+char title_font[MAX_LEN];
+int font_size = 20;
 Window overlay;
 
 static int wait = 0;
@@ -28,19 +33,19 @@ void draw(void** args) {
 	if(title == NULL){
 		return;
 	}
-	cairo_select_font_face(cr, "Sans Regular",CAIRO_FONT_SLANT_NORMAL,CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size(cr, 20);
+	cairo_select_font_face(cr, title_font,CAIRO_FONT_SLANT_NORMAL,CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_font_size(cr, font_size);
 	cairo_text_extents_t extents;
 	cairo_text_extents(cr, title, &extents);
 	cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
 	for(float i = 0; i < 1.57f; i+= 0.1f){
 		cairo_set_source_rgba(cr, 0, 0, 0, 0.4 * sin(i));
 		cairo_move_to(cr,0,0);
-    		cairo_rectangle(cr, 0, 0, width, 50);
+    		cairo_rectangle(cr, 0, 0, width, height);
 		
     		cairo_fill(cr);
 		cairo_set_source_rgba(cr, 1, 1, 1, sin(i));
-		cairo_move_to(cr,width/2 - extents.width/2,25);
+		cairo_move_to(cr,width/2 - extents.width/2,height/2);
 		cairo_show_text(cr,title);
 		XFlush(display);
 		usleep(10000);
@@ -55,11 +60,11 @@ void draw(void** args) {
 	for(float i = 1.57f; i < 3.14f; i+= 0.1f){
 		cairo_set_source_rgba(cr, 0, 0, 0, 0.4 * sin(i));
 		cairo_move_to(cr,0,0);
-    	cairo_rectangle(cr, 0, 0, width, 50);
+    	cairo_rectangle(cr, 0, 0, width, height);
 		
     	cairo_fill(cr);
 		cairo_set_source_rgba(cr, 1, 1, 1, sin(i));
-		cairo_move_to(cr,width/2 - extents.width/2,25);
+		cairo_move_to(cr,width/2 - extents.width/2,height/2);
 		cairo_show_text(cr,title);
 		XFlush(display);
 		usleep(10000);
@@ -72,12 +77,7 @@ void draw(void** args) {
 
 char* format_string(DB_playItem_t* it){
 	ddb_playlist_t * nowplaying_plt = deadbeef->plt_get_curr ();
-	char* code_script = NULL;
-	if(deadbeef->pl_find_meta(it,"title") == 0){
-		code_script = deadbeef->tf_compile("%title%");
-	}else{
-		code_script = deadbeef->tf_compile("%artist% - %title%");
-	}
+	char* code_script = deadbeef->tf_compile(title_format);
 	
 	ddb_tf_context_t context;
 	context._size = sizeof(ddb_tf_context_t);
@@ -90,9 +90,9 @@ char* format_string(DB_playItem_t* it){
     context.update = 0;
     context.dimmed = 0;
 
-	char * out = malloc(256);
+	char * out = malloc(MAX_LEN);
 
-	deadbeef->tf_eval (&context, code_script, out, 256);
+	deadbeef->tf_eval (&context, code_script, out, MAX_LEN);
 	deadbeef->tf_free (code_script);
 	deadbeef->plt_unref (nowplaying_plt);
 
@@ -107,7 +107,7 @@ static void window_thread(void* arg){
 
 	overlay = XCreateWindow(
 	        display, root,
-	        0, 0, width, 50, 0,
+	        0, 0, width, height, 0,
 	        vinfo.depth, InputOutput, 
 	        vinfo.visual,
 	        CWOverrideRedirect | CWColormap | CWBackPixel | CWBorderPixel, &attrs
@@ -116,7 +116,7 @@ static void window_thread(void* arg){
 	XMapWindow(display, overlay);
 	cairo_surface_t* surf = cairo_xlib_surface_create(display, overlay,
                                   vinfo.visual,
-                                  width, 50);
+                                  width, height);
 	cairo_t* cr = cairo_create(surf);
 	//draw(cr,title);
 	void* args[] = {cr,title};
@@ -160,6 +160,13 @@ static int message(uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2){
 	if(id == DB_EV_SONGCHANGED){
 		show_overlay();
 	}
+	if (id == DB_EV_CONFIGCHANGED)
+	{
+		height = deadbeef->conf_get_int("overlay.height",50);
+		deadbeef->conf_get_str("overlay.title","%artist% :: %title% $if(%album%,. from )%album% :: %length%\"",title_format,MAX_LEN);
+		font_size = deadbeef->conf_get_int("overlay.font_size",20);
+		deadbeef->conf_get_str("overlay.font_name","Sans Regular",title_font,MAX_LEN);
+	}
 }
 
 static int start(){
@@ -201,8 +208,11 @@ static DB_plugin_action_t* get_actions(DB_playItem_t *it){
 	return &show_action;
 }
 
-static const char settings_dlg[] = "property \"height\" entry overlay.height \"50\""
-"";
+static const char settings_dlg[] = 
+"property \"height\" entry overlay.height \"50\";\n"
+"property \"title\" entry overlay.title \"%artist% :: %title% $if(%album%,. from )%album% :: %length%\";\n"
+"property \"Font size\" entry overlay.font_size \"20\";\n"
+"property \"Font\" entry overlay.font_name \"Sans Regular\";\n";
 
 static DB_misc_t plugin = {
     .plugin = {
@@ -210,7 +220,7 @@ static DB_misc_t plugin = {
         .api_vminor = 10,
         .id = NULL,
         .name = "X11 overlay",
-        .descr = "show overlay song info",
+        .descr = "show overlay song info\ntitle format help:https://github.com/DeaDBeeF-Player/deadbeef/wiki/Title-formatting-2.0",
         .copyright = "Murloc Knight",
         .website = "https://github.com/KnightMurloc/DeadBeef-X11-Overlay-Plugin-",
 
